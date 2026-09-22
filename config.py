4404dd0dd9f71aa94c9e675e182371e74c33c7cd
Raphael Mcharo <raphael@example.com>
2026-09-22 20:46:17 +0300

    Use environment variable for database

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://shopuser:Shop%4012345@localhost/shop_management"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "mysql+pymysql://shopuser:Shop%4012345@localhost/shop_management"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
