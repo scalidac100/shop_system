b614995341e3a3bf56336678cb7f2a86b31f2825
Raphael Mcharo <raphael@example.com>
2026-09-23 13:46:29 +0300

    Fix MySQL PyMySQL connection

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "mysql+pymysql://shopuser:Shop%4012345@localhost/shop_management"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
